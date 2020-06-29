0a5e6975407d6412d3ce42423782d00d48fe2fa1
Abubakar Abid <a12d@stanford.edu>
2020-06-29 10:39:22 -0500

    Update run-gradio.py

--- run-gradio.py
+++ run-gradio.py
@@ -1,6 +1,6 @@
 from imagenetlabels import idx_to_labels
 import tensorflow as tf
-from gradio import inputs, outputs
+import gradio as gr
 
 mobile_net = tf.keras.applications.MobileNetV2()
 
@@ -10,8 +10,8 @@ def classify_image(inp):
     return {idx_to_labels[i].split(',')[0]: float(prediction[i]) for i in range(1000)}
 
 
-imagein = inputs.Image(shape=(224, 224, 3))
-label = outputs.Label(num_top_classes=3)
+imagein = gr.inputs.Image(shape=(224, 224, 3))
+label = gr.outputs.Label(num_top_classes=3)
 
 gr.Interface(
     classify_image, 
