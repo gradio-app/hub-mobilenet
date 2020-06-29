f7178d904509e2aedea351553a9f6ade338760db
Abubakar Abid <a12d@stanford.edu>
2020-06-29 10:25:39 -0500

    Update make-gradio.py

--- make-gradio.py
+++ make-gradio.py
@@ -4,15 +4,10 @@ from gradio import inputs, outputs
 
 mobile_net = tf.keras.applications.MobileNetV2()
 
-graph = tf.get_default_graph()
-sess = tf.keras.backend.get_session()
-
 def classify_image(inp):
-    with graph.as_default():
-        with sess.as_default():
-            inp = inp.reshape((1, 224, 224, 3))
-            prediction = mobile_net.predict(inp).flatten()
-            return {idx_to_labels[i].split(',')[0]: float(prediction[i]) for i in range(1000)}
+    inp = inp.reshape((1, 224, 224, 3))
+    prediction = mobile_net.predict(inp).flatten()
+    return {idx_to_labels[i].split(',')[0]: float(prediction[i]) for i in range(1000)}
 
 
 imagein = inputs.Image(shape=(224, 224, 3))
@@ -21,4 +16,5 @@ label = outputs.Label(num_top_classes=3)
 gr.Interface(
     classify_image, 
     imagein, 
-    label).launch();
+    label,
+    capture_session=True).launch();
